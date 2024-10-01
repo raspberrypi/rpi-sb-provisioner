d5ad749b1dad0af0439c2d8c1656895093a2d863
Cody Scott <cody.scott@giatec.ca>
2024-10-01 09:15:34 -0400

    Cleanup validator

--- config/validator.py
+++ config/validator.py
@@ -1,36 +1,30 @@
 ## Format of return will be [Happy: bool, error: str]
-from os import path
+import os
 import subprocess
 
+
 def validate_CUSTOMER_KEY_FILE_PEM(text) -> tuple[bool, str]:
-    if path.exists(text):
-        pass
-    else:
+    if not os.path.exists(text):
         return (False, "Could not find file " + text)
     output = subprocess.run(["openssl", "rsa", "-in", text, "-check", "-noout"], capture_output=True)
-    if "RSA key ok" in output.stdout.decode():
-        pass
-    else:
+    if "RSA key ok" not in output.stdout.decode():
         return (False, "openssl error: " + output.stdout.decode() + output.stderr.decode())
-    # "openssl rsa -in " + text + "  -check -noout"
     return (True, "")
+
 
 def validate_CUSTOMER_KEY_PKCS11_NAME(text) -> tuple[bool, str]:
     output = subprocess.run(["openssl", "rsa", text, "-engine", "pkcs11", "-keyform", "engine", "-check", "-noout"], capture_output=True)
-    if "RSA key ok" in output.stdout.decode():
-        pass
-    else:
+    if "RSA key ok" not in output.stdout.decode():
         return (False, "openssl error: " + output.stdout.decode() + output.stderr.decode())
-    # "openssl rsa -in " + text + "  -check -noout"
     return (True, "")
 
+
 def validate_GOLD_MASTER_OS_FILE(text) -> tuple[bool, str]:
-    if path.exists(text):
-        pass
-    else:
+    if not os.path.exists(text):
         return (False, "Could not find file " + text)
 
     return (True, "")
+
 
 def validate_RPI_DEVICE_STORAGE_TYPE(text) -> tuple[bool, str]:
     if text in "sd nvme emmc":
@@ -38,36 +32,38 @@ def validate_RPI_DEVICE_STORAGE_TYPE(text) -> tuple[bool, str]:
     else:
         return (False, "type `" + text + "` was not any of sd, nvme or emmc")
 
+
 def validate_RPI_DEVICE_FAMILY(text) -> tuple[bool, str]:
     if text in "45":
         return (True, "")
     else:
         return (False, "type `" + text + "` was not any of 4 or 5")
 
+
 def validate_RPI_DEVICE_BOOTLOADER_CONFIG_FILE(text) -> tuple[bool, str]:
-    if path.exists(text):
-        pass
-    else:
+    if not os.path.exists(text):
         return (False, "Could not find file " + text)
 
     return (True, "")
 
+
 def validate_RPI_DEVICE_LOCK_JTAG(text) -> tuple[bool, str]:
     return (True, "")
+
 
 def validate_RPI_DEVICE_EEPROM_WP_SET(text) -> tuple[bool, str]:
     return (True, "")
 
+
 def validate_RPI_DEVICE_FETCH_METADATA(text) -> tuple[bool, str]:
     return (True, "")
+
 
 def validate_DEMO_MODE_ONLY(text) -> tuple[bool, str]:
     return (True, "")
 
+
 def validate_RPI_SB_WORKDIR(text) -> tuple[bool, str]:
-    if len(text) > 0:
-        if text[0] == "/":
-            pass
-        else:
-            return (False, "Please specify absolute path, beginning with /")
+    if text and text[0] != "/":
+        return (False, "Please specify absolute path, beginning with /")
     return (True, "")
