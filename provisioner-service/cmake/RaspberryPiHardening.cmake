fd1d6a8a76c05508ae99a8cfab8ce7f81479dfbd
Tom Dewey <tom.dewey@raspberrpi.com>
2025-04-04 14:34:49 +0100

    hardening: Add branch protection

--- provisioner-service/cmake/RaspberryPiHardening.cmake
+++ provisioner-service/cmake/RaspberryPiHardening.cmake
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.25)
 
 set(RPI_COMPILER_SECURITY_FLAGS 
-    "-Wformat -Wformat=2 -Werror=format-security -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3 -D_GLIBCXX_ASSERTIONS -fstack-protector-strong -fstack-clash-protection")
+    "-Wformat -Wformat=2 -Werror=format-security -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3 -D_GLIBCXX_ASSERTIONS -fstack-protector-strong -fstack-clash-protection -mbranch-protection=standard")
 
 set(RPI_EXE_LINKER_FLAGS
     "-Wl,-z,relro,-z,now -Wl,-z,nodlopen -Wl,-z,noexecstack -Wl,--as-needed -Wl,--no-copy-dt-needed-entries")
