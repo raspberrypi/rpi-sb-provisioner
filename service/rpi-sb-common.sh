89c0a8af8fbeb3174116f5df7b07356b8a762802
Tom Dewey <tom.dewey@raspberrpi.com>
2025-07-16 13:13:14 +0100

    bootstrap: Enable device detection customisation scripts

--- service/rpi-sb-common.sh
+++ service/rpi-sb-common.sh
@@ -201,6 +201,13 @@ run_customisation_script() {
         if [ "${STAGE_NAME}" = "post-flash" ]; then
             # For post-flash stage, pass device info that can be used with fastboot
             "${SCRIPT_PATH}" "${FASTBOOT_DEVICE_SPECIFIER}" "${TARGET_DEVICE_SERIAL}" "${RPI_DEVICE_STORAGE_TYPE}"
+        elif [ "${STAGE_NAME}" = "bootstrap" ]; then
+            # For bootstrap stage, pass device detection info
+            TARGET_DEVICE_SERIAL_ARG="$3"
+            TARGET_DEVICE_FAMILY_ARG="$4"
+            TARGET_USB_PATH_ARG="$5"
+            TARGET_DEVICE_PATH_ARG="$6"
+            "${SCRIPT_PATH}" "${TARGET_DEVICE_SERIAL_ARG}" "${TARGET_DEVICE_FAMILY_ARG}" "${TARGET_USB_PATH_ARG}" "${TARGET_DEVICE_PATH_ARG}"
         else
             # For filesystem mount stages, pass mount points
             BOOT_MOUNT="$3"
